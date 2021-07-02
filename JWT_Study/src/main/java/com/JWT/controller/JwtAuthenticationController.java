674ab195dfc26ade590ce4a73e265bff2884f373
KIM-JS-95 <baugh248730@gmail.com>
2021-07-02 14:58:24 +0900

    JWT

---
+++ JWT_Study/src/main/java/com/JWT/controller/JwtAuthenticationController.java
@@ -0,0 +1,51 @@
+package com.JWT.controller;
+
+import com.JWT.config.JwtTokenUtil;
+import com.JWT.member.Member;
+import com.JWT.service.JwtUserDetailsService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        final Member member = userDetailService.authenticateByEmailAndPassword
+                (authenticationRequest.getEmail(), authenticationRequest.getPassword());
+        final String token = jwtTokenUtil.generateToken(member.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+}
+
+@Data
+class JwtRequest {
+
+    private String email;
+    private String password;
+
+}
+
+@Data
+@AllArgsConstructor
+class JwtResponse {
+
+    private String token;
+
+}
+
